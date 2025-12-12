8c0ac1c41f333161f1a1f4d72501aae719ef7b84
Yasmine <yasmineharroudi2020@gmail.com>
2025-12-12 15:43:22 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import "./index.css"
 //import Card from "./Card";
 //import Button from "./Button/Button";
 //import Student from "./Props/Student"
-import UserGreeting from './conditional_rendering/UserGreeting'
-
+//import UserGreeting from './conditional_rendering/UserGreeting'
+// import List from './Rendering_List/List'
+//import Lists from './Rendering_List/Lists'
+import Button from "./click_event/Button";  
 
 //Components-------------------------------------------------------
 
@@ -63,6 +65,63 @@ import UserGreeting from './conditional_rendering/UserGreeting'
 //         </>
 //     );
 // }
-// export default App;
 
-//Rendering lists
+
+//List using map(), sort(), filter -----------------------------------------------------------//
+
+// function App(){
+//     return(
+//         <>
+//         <List/>
+//         </>
+//     );
+// }
+
+//Lists using App()-----------------------------------------------------------//
+
+// function App(){
+//     const fruits = [{id:1, name:'apple', calories: 95},
+//                 {id:2, name:'orange', calories: 45},
+//                 {id:3, name: 'banana', calories: 105},
+//                 {id:4, name:'coconut', calories: 159},
+//                 {id:5, name: 'pineapple', calories:37}];
+
+//     const vegetables = [{id:6, name:'potatoes', calories: 110},
+//                         {id:7, name:'celery', calories: 15},
+//                         {id:8, name: 'carrots', calories: 25},
+//                         {id:9, name:'corn', calories: 63},
+//                         {id:10, name: 'broccoli', calories:50}];
+//     return(
+//         <>
+//         {fruits.length>0 && <Lists items={fruits} category='Fruits'/>}
+//         <Lists items={vegetables} category='Vegetables'/>
+//         </>
+
+//     );
+// }
+
+function App(){
+    return(<>
+    <Button/>
+    </>)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ export default App;
