3c0124c5542de183da22b226a09c10b77869586e
Yasmine <yasmineharroudi2020@gmail.com>
2025-12-20 11:53:12 +0100

    .

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,7 +10,7 @@ function Product(props){
                   <p className="card-text">{props.data.price}</p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Ajouter au panier</button>
                     </div>
               </div>
             </div> 
