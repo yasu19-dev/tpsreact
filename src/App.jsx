285adaa64c3278c7d739f24b36fb79e8e75d6faa
Yasmine <yasmineharroudi2020@gmail.com>
2025-12-20 12:35:36 +0100

    TP_incrementer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-   <Products/>
+import { Component } from "react"
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state ={num:0};
+    }
+    inc = () => {
+        this.setState({num: this.state.num+1});
+    };
+    dec = () => {
+        this.setState({num: this.state.num-1});
+    };
+    render(){
+        return(
+            <div className="App">
+                <p>{this.state.num}</p>
+                <button onClick={this.inc}>Incrémenter</button>
+                <button onClick={this.dec}>Derementer</button>
+            </div>
+  )
+    }
+  
 }
 
 export default App
