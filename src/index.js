73a10c78c0d19d6ff23a49175f200b60efc363ff
Yasmine <yasmineharroudi2020@gmail.com>
2026-02-08 13:43:30 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Notice the '/client' here
 import App from './App';
 import './styles/index.scss';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+
 
 // 1. Get the DOM element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true, // Enable Redux DevTools
+});
+
 // 2. Render the App inside it
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    // Here we will add our reducers
+});
